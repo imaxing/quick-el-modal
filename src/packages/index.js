6a34e47e5d185c6c5aab53cee3f88ab4b9db98cb
gexing <imaxing@126.com>
2022-05-10 14:21:54 +0800

    upt: 修复无法使用store/router等功能问题

--- src/packages/index.js
+++ src/packages/index.js
@@ -1,19 +1,25 @@
-import Vue from 'vue'
 import QuickElModal from './quick-el-modal'
+export default {
+  install(Vue, opts = {}) {
+    Vue.prototype[opts.name || 'quickModal'] = options => {
+      const ModalInstance = Vue.extend(QuickElModal)
+      ModalInstance.prototype.$store = opts.store
+      ModalInstance.prototype.$router = opts.router
+      ModalInstance.prototype.$route = opts.route
+      const instance = new ModalInstance({
+        data: {
+          ...options,
+          destroy: () => instance.$el.remove()
+        }
+      })
 
-export default (options = {}) => {
-  const QuickElModalConstructor = Vue.extend(QuickElModal)
-  const instance = new QuickElModalConstructor({
-    data: {
-      ...options,
-      destroy: () => instance.$el.remove()
+      instance.$mount()
+      document.body.appendChild(instance.$el)
+      instance.$nextTick(() => {
+        instance.visible = true
+      })
+
+      return instance
     }
-  })
-  instance.$mount()
-  document.body.appendChild(instance.$el)
-  instance.$nextTick(() => {
-    instance.visible = true
-  })
-
-  return instance
+  }
 }
